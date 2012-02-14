192738a018439e3b5c69c5f05c3881be9506763b
Chris Winslett <chris@mongohq.com>
2012-02-14 13:23:11 -0600

    Added calculations common to mongostat output.

--- lib/mongohq/member.rb
+++ lib/mongohq/member.rb
@@ -1,5 +1,17 @@
 module MongoHQ
   class Member < Base
 
+    def index_misses_percentage
+      value = send("indexCounters/btree/misses".to_sym) / send("indexCounters/btree/accesses".to_sym)
+      value.nan? ? 0 : value
+    end
+
+    def queue_lengths
+      "#{send("globalLock/currentQueue/readers".to_sym)}|#{send("globalLock/currentQueue/writers".to_sym)}"
+    end
+
+    def queue_lengths
+      "#{send("globalLock/activeClients/readers".to_sym)}|#{send("globalLock/activeClients/writers".to_sym)}"
+    end
   end
 end
