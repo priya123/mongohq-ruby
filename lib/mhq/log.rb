e873da8436b3168d46ef9742a9a08f8d007c9206
Chris Winslett <chris@mongohq.com>
2012-02-16 18:46:20 -0600

    Added ability to filter logs by host.

--- lib/mhq/log.rb
+++ lib/mhq/log.rb
@@ -3,14 +3,16 @@ module Mhq
 
     desc "show <database name>", "Show logs for database"
     method_option :host, :aliases => "-h", :desc => "Hostname to restrict", :type => :string
-    def show(database_name, host = nil)
-      host ||= options.host
+    def show(database_name, filter_host = nil)
+      filter_host ||= options.host
 
       logs = []
       db = MongoHQ::Database.find(database_name)
       db.logs.members.each do |member|
         host = (member.host.split(/[\.\:]/) - db.deployment_path.split(/[\.\:]/)).join(".")
         host == "" ? db.name.strip : host
+
+        next if !filter_host.nil? && host != filter_host
 
         latest_log = "\n" + member.log
         {"Jan" => "01", "Feb" => "02", "Mar" => "03", "Apr" => "04", "May" => "05", "Jun" => "06", "Jul" => "07", "Aug" => "08", "Sep" => "09", "Oct" => "10", "Nov" => "11", "Dec" => "12"}.each do |find, replace|
